f2e7ed113c7a5c5a2d9915310147c944489e6bd8
Guilherme Marciano <guipicini@hotmail.com>
2017-11-21 01:55:20 -0200

    tirei a /

--- vue/src/components/pages/loginPage.vue
+++ vue/src/components/pages/loginPage.vue
@@ -29,10 +29,10 @@
         name: 'loginPage',
         methods: {
             checkFacebook: function () {
-                window.location.href = `${window.location.href}/connect/facebook`;
+                window.location.href = `${window.location.href}connect/facebook`;
             },
             checkGoogle: function () {
-                window.location.href = `${window.location.href}/connect/google`;
+                window.location.href = `${window.location.href}connect/google`;
             }
         }
     }
